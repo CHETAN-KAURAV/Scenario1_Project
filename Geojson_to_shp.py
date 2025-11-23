962666d4783cc7929a873e414e82e7f677b75761
Chetan Kaurav <chetankaurav9@gamil.com>
2025-11-23 12:48:53 +0530

    Initial Commit

--- Geojson_to_shp.py
+++
@@ -1,7 +0,0 @@
-import geopandas as gpd
-
-# Load your GeoJSON
-gdf = gpd.read_file("data\delhi_ncr_region.geojson")
-
-# Save as Shapefile
-gdf.to_file("delhi_ncr_region_shapefile.shp", driver="ESRI Shapefile")
--- add_path_to_csv.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-import pandas as pd
-
-df = pd.read_csv("data/image_coords.csv")
-df['path'] = df['filename'].apply(lambda fn: str(Path('data/rgb') / fn))
-df.to_csv("data/image_coords_with_path.csv", index=False)
